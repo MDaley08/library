6bf7c5075c9b4d4fd955121fb9b7be33efa14ad8
Michael Daley <mgd3@njit.edu>
2024-05-19 17:33:08 -0400

    completed book add and display funtionality, added ability to add to books to library using forms

--- js/app.js
+++ js/app.js
@@ -1,13 +1,13 @@
 const myLibrary = [];
 
-function Book(title, author, pages){
-
+function Book(title, author, pages,read=false){
     let errorString = "Book: input invalid, you have entered invalid: "
     let errors = [];
 
     if(typeof title !== 'string') errors.push('title'); 
     if(typeof author !== 'string') errors.push('author');
     if(typeof pages !== 'number') errors.push('pages');
+    if(typeof read !== 'boolean') errors.push('read');
 
     if(errors.length){
         console.log(errorString + errors.join(", "));
@@ -17,7 +17,7 @@ function Book(title, author, pages){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = false;
+    this.read = read;
     this.info = () => {
         return [this.title, this.author, this.pages,this.read];
     }
@@ -43,26 +43,112 @@ const createBookElement = (book) => {
     bookElement.classList.add('book');
 
     let title = document.createElement('h3');
+
     let author = document.createElement('p');
+    let authorText = document.createElement('span');
+
     let pages = document.createElement('p');
+    let pagesText = document.createElement('span');
+
+    let bookButtons = document.createElement('div');
+    let readBook = document.createElement('button');
+    let deleteBook = document.createElement('button');
+
+    bookButtons.appendChild(deleteBook);
+    bookButtons.appendChild(readBook);
 
     title.textContent = book.title;
-    author.textContent = `Author: ${book.author}`;
-    pages.textContent = `Pages: ${book.pages}`;
 
+    author.textContent = 'Author: ';
+    authorText.textContent = book.author;
+    author.appendChild(authorText);
+
+    pages.textContent = 'Pages: ';
+    pagesText.textContent = book.pages;
+    pages.appendChild(pagesText);
+
+    bookButtons.classList.add('book-buttons');
+    readBook.classList.add('read-btn');
+    deleteBook.classList.add('delete-btn');
+
+    if(book.read){
+        readBook.textContent = 'Read';
+        readBook.style.backgroundColor = 'darkolivegreen';
+    }
+    else{
+        readBook.textContent = 'Unread';
+        readBook.style.backgroundColor = 'maroon';
+    }
+
+    deleteBook.textContent = 'delete';
+    
     bookElement.appendChild(title);
     bookElement.appendChild(author);
     bookElement.appendChild(pages);
+    bookElement.appendChild(bookButtons);
+
+    deleteBook.addEventListener('click', () => {
+        bookElement.remove();
+        myLibrary.splice(myLibrary.indexOf(book));
+    });
+
+    readBook.addEventListener('click', () => {
+         readBook.classList.toggle('read');
+         if(readBook.classList.contains('read')){
+            readBook.style.backgroundColor = 'darkolivegreen';
+            readBook.textContent = 'Read';
+            book.read = true;
+         }
+         else{
+            readBook.style.backgroundColor = 'maroon'
+            readBook.textContent = 'Unread'
+            book.read = false;
+         }
+    });
 
     return bookElement;
 }
-let testbook = new Book('Catching a mocking bird','test',123)
-let bookContainer = document.getElementById('book-container');
-bookContainer.appendChild(createBookElement(testbook));
-bookContainer.appendChild(createBookElement(testbook));
-bookContainer.appendChild(createBookElement(testbook));
-bookContainer.appendChild(createBookElement(testbook));
 
 const displayBooks = ()=> {
-
+    let bookContainer = document.getElementById("book-container");
+    for(let i = 0; i < myLibrary.length; i++){
+        if(!myLibrary[i].displayed){
+            bookContainer.appendChild(createBookElement(myLibrary[i]));
+        }
+    }
 }
+
+const updateLibrary = (book)=> {
+    let bookContainer = document.getElementById("book-container");
+    bookContainer.appendChild(createBookElement(book));
+    addBookToLibrary(book);
+}
+
+// form event listners 
+let dialog = document.querySelector("dialog");
+let form = document.querySelector("form");
+let title = document.getElementById('title');
+let author = document.getElementById('author');
+let pages = document.getElementById('pages');
+let read = document.getElementById('read');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    let book = new Book(title.value,author.value,parseInt(pages.value),read.checked);
+    updateLibrary(book);
+    form.reset();
+    dialog.close();
+});
+
+let cancel = document.querySelector("form .delete-btn");
+cancel.addEventListener('click', () => {
+    form.reset();
+    dialog.close();
+})
+
+let addBookBtn = document.getElementById("add-book");
+addBookBtn.addEventListener('click', () => {
+    dialog.showModal();
+});
+
+displayBooks();
